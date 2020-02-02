75bd7634a58ffddc7411144204e9f1c37f0ed949
Valery Kotov <valery.kotov@esrlabs.com>
2020-02-02 15:31:31 +0100

    Add simple resumable type example

---
+++ compile.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+clang++-9 -stdlib=libc++ -std=c++2a -Wall resumable_type.cpp -o resumable_type
---
+++ resumable_type.cpp
@@ -0,0 +1,47 @@
+#include <experimental/coroutine>
+#include <iostream>
+
+class resumable {
+public:
+    struct promise_type;
+    using coroutine_handle = std::experimental::coroutine_handle<promise_type>;
+
+    resumable(coroutine_handle handle) : handle_(handle) { assert(handle_); }
+    resumable(resumable&) = delete;
+    resumable(resumable&&) = delete;
+
+    ~resumable() { handle_.destroy(); }
+
+    bool resume() {
+        if (not handle_.done())
+            handle_.resume();
+        return not handle_.done();
+    }
+private:
+    coroutine_handle handle_;
+};
+
+struct resumable::promise_type {
+    using coroutine_handle = std::experimental::coroutine_handle<promise_type>;
+    auto get_return_object() {
+        return coroutine_handle::from_promise(*this);
+    }
+    auto initial_suspend() { return std::experimental::suspend_always(); }
+    auto final_suspend() { return std::experimental::suspend_always(); }
+    void return_void() {}
+    void unhandled_exception() {
+        std::terminate();
+    }
+};
+
+resumable foo(){
+    std::cout << "Hello" << std::endl;
+    co_await std::experimental::suspend_always();
+    std::cout << "World" << std::endl;
+}
+
+int main() {
+    resumable res = foo();
+    while (res.resume());
+    return 0;
+}
